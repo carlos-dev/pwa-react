845bf4dd40f913f87da49bd68d9e0ef4fd1f8761
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-04-10 12:16:53 -0300

    estilizando o componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Header, Repositories, GlobalStyle } from "./styles";
+
+export class App extends Component {
+  state = {
+    newRepoInput: "",
+    repositories: JSON.parse(localStorage.getItem("repositories")),
+  };
+
+  addRepository = async () => {
+    const { newRepoInput } = this.state;
+
+    if (!newRepoInput) return;
+
+    const response = await axios.get(
+      `https://api.github.com/repos/${newRepoInput}`
+    );
+
+    this.setState({
+      newRepoInput: "",
+      repositories: [...this.state.repositories, response.data],
+    });
+
+    localStorage.setItem(
+      "repositories",
+      JSON.stringify(this.state.repositories)
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <GlobalStyle />
+          <Header>
+            <input
+              placeholder="adicionar repositório"
+              onChange={(e) => this.setState({ newRepoInput: e.target.value })}
+              value={this.state.newRepoInput}
+            />
+
+            <button onClick={this.addRepository}>Adicionar</button>
+          </Header>
+
+          <Repositories>
+            {this.state.repositories.map((repository) => (
+              <li key={repository.id}>
+                <img src={repository.owner.avatar_url} />
+                <div>
+                  <strong>{repository.name}</strong>
+                  <p>{repository.description}</p>
+                  <a href={repository.html_url}>Acessar</a>
+                </div>
+              </li>
+            ))}
+          </Repositories>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
